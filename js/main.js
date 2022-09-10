b148a7821104bd055c89b75afcc9d5ca84e74f1f
unknown <https://github.com/Umid-Olimovich>
2022-09-10 13:51:13 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
-let salaries = {
-	"Akmal": 1200,
-	"Salim": 5200,
-	"Karima": 1800
-}
-let sum = 0;
-function sumSalaries(salaries){
-    let values = Object.values(salaries)
-    for (let key of values) {
 
-        console.log(key);
-        sum += key;
+let arr = [];
+let count = 0;
+
+function sumInput() {
+  for (;;) {
+    var sum = +prompt("son kiriting")  ;
+    if (sum == "" || sum == "") {
+     break;
     }
-    return sum ;
+     else  {
+        
+    count += sum;
+    arr.push(sum);
+    console.log(arr);
+      
+    }
+  }
 
 }
-alert(sumSalaries(salaries));
+sumInput()
+alert(count)
+
+//let salaries = {
+// 	"Akmal": 1200,
+// 	"Salim": 5200,
+// 	"Karima": 1800
+// }
+// let sum = 0;
+// function sumSalaries(salaries){
+//     let values = Object.values(salaries)
+//     for (let key of values) {
+
+//         console.log(key);
+//         sum += key;
+//     }
+//     return sum ;
+
+// }
+// alert(sumSalaries(salaries));
